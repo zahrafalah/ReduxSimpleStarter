188b37de21c279bcf0280468f4c53c9140532b1f
zahra falah <sepidehfallah2@gmail.com>
2018-10-24 15:17:10 -0700

    refactoring the api search in a class base component

--- reduxsimplestarter/src/App.js
+++ reduxsimplestarter/src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/search_bar';
 
-// import dotenv from 'dotenv';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyC4f-CdOH7kwmwpg3LsjPVgiWCyyMx95KU';
+//call to YouTube API to call to get some info,
+// the first arg would be a search term, we pass an obj with key and a term
+// 2nd arg is the call back func
+// YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+//   console.log(data);
+// });
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+      //this.setState({videos : videos })jsx syntax
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <SearchBar />
--- reduxsimplestarter/src/index.js
+++ reduxsimplestarter/src/index.js
@@ -4,14 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import YTSearch from 'youtube-api-search';
-const API_KEY = 'AIzaSyC4f-CdOH7kwmwpg3LsjPVgiWCyyMx95KU';
-//call to YouTube API to call to get some info,
-// the first arg would be a search term, we pass an obj with key and a term
-// 2nd arg is the call back func
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-  console.log(data);
-});
 //Take this component's generated HTML and put it on the page(In the DOM)
 ReactDOM.render(<App />, document.getElementById('root'));
 
