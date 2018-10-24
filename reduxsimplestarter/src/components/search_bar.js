3e9ec52cf4be6b6e283ac3647fabec6062dedad5
zahra falah <sepidehfallah2@gmail.com>
2018-10-24 13:14:45 -0700

    handle events and state objects

--- reduxsimplestarter/src/components/search_bar.js
+++ reduxsimplestarter/src/components/search_bar.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+  }
+  // handleChange(event) {
+  //   console.log(event.target.value);
+  // }
   render() {
-    return <input />;
+    // return <input onChange={this.handleChange} />;
+    //or
+    // return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        {/* value of the state : {this.state.term} */}
+      </div>
+    );
   }
 }
 
