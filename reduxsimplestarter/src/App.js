0e465fedae3050aaf7f125c79f2c8c45590c3278
zahra falah <sepidehfallah2@gmail.com>
2018-10-23 17:21:04 -0700

    class base component vs fuctional components

--- reduxsimplestarter/src/App.js
+++ reduxsimplestarter/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/search_bar';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello there</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
 
