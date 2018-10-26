1b54f48ae5593294103dfb017e695b3720ffa72e
zahra falah <sepidehfallah2@gmail.com>
2018-10-26 11:07:27 -0700

    Array items key

--- reduxsimplestarter/src/components/video_list.js
+++ reduxsimplestarter/src/components/video_list.js
@@ -2,10 +2,13 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = props => {
+  //loop through the array of videos coming from API
+  //for each element of videos the function will call with a single video
+  //and we return a VideoListItem, and we will pass it a video as a property named video
   const videoItems = props.videos.map(video => {
-    return <VideoListItem video={video} />;
+    return <VideoListItem key={video.etag} video={video} />;
   });
-
+  //we are using JS variable so we need curly braces
   return <ul>{videoItems}</ul>;
 };
 
