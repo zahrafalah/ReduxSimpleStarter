793e270b05095005d6945116e5d59528316e8751
zahra falah <sepidehfallah2@gmail.com>
2018-10-25 17:55:36 -0700

    building lists with Map

--- reduxsimplestarter/src/components/video_list.js
+++ reduxsimplestarter/src/components/video_list.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = props => {
-  return <ul>{props.videos.length}</ul>;
+  const videoItems = props.videos.map(video => {
+    return <VideoListItem video={video} />;
+  });
+
+  return <ul>{videoItems}</ul>;
 };
 
 export default VideoList;
