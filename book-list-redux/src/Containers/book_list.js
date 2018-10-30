8cf67fb13e898b598314902b007fd078d4e6c682
zahra falah <sepidehfallah2@gmail.com>
2018-10-30 16:03:24 -0700

    reducers, Containers

---
+++ book-list-redux/src/Containers/book_list.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class bookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li key={book.title} className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
+  }
+  render() {
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
+}
+
+export default bookList;
---
+++ book-list-redux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The good parts' },
+    { title: 'Harry Potter' },
+    { title: 'Eloquent Ruby' },
+    { title: 'Dark castle' }
+  ];
+}
