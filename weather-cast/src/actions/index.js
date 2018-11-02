02c31df9520f311b47f5f5265e593b58543fcf31
zahra falah <sepidehfallah2@gmail.com>
2018-11-02 12:10:31 -0700

    root url added

--- weather-cast/src/actions/index.js
+++ weather-cast/src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = 'd6d81d6eeca4ea40404e55fbb83365c4';
+// URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+//or
+ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+//Action creators always have to create action that return an object
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  //An action is an obj that always have a type
+  return {
+    type: FETCH_WEATHER
+  };
+}
--- weather-cast/src/index.js
+++ weather-cast/src/index.js
@@ -6,27 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxPromise from 'redux-promise';
-import reducers from './reducers';
+import rootReducer from './';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   //telling redux where the reducers are
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(rootReducer)}>
     <App />
   </Provider>,
 
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
