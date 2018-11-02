2aeadee5865bac1f9b5354295fb555d8bf51b6a7
zahra falah <sepidehfallah2@gmail.com>
2018-11-02 12:27:41 -0700

    make ajax call with axios, installing axios

--- weather-cast/src/actions/index.js
+++ weather-cast/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = 'd6d81d6eeca4ea40404e55fbb83365c4';
 // URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 //or
@@ -7,8 +9,14 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 //Action creators always have to create action that return an object
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  //An action is an obj that always have a type
+  //making Ajax call by axios: take the url above and make a get request.
+  //then return it as a promise(request) then pass it to payload
+  const request = axios.get(url);
+
+  //An action is an obj that always have a type. but payload is an extra
+  //info that always contains more info about that particular action.
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
