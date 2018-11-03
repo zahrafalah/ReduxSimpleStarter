8a9238e625f3858e47928251bdf07db6d57e93d4
zahra falah <sepidehfallah2@gmail.com>
2018-11-02 18:12:16 -0700

    building a list container

--- weather-cast/src/App.js
+++ weather-cast/src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './containers/search_bar';
+import WeatherList from './containers/weather_list';
+import { Grid, Segment, Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
-    return <SearchBar />;
+    return (
+      <Container>
+        <Segment style={{ padding: '2em 0em' }}>
+          <Grid container stackable>
+            <Grid.Row>
+              <Grid.Column textAlign="center" width={16}>
+                <SearchBar />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment style={{ padding: '1em' }}>
+          <Grid container>
+            <Grid.Row>
+              <Grid.Column textAlign="center" width={16}>
+                <WeatherList />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </Container>
+    );
   }
 }
 
 export default App;
+
+{
+  /* <div>
+<SearchBar />
+<WeatherList />
+</div> */
+}
---
+++ weather-cast/src/containers/weather_list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Table } from 'semantic-ui-react';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <Table inverted>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>City</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
+            <Table.HeaderCell>Notes</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>John</Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+            <Table.Cell>None</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Jamie</Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+            <Table.Cell>Requires call</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell>3 People</Table.HeaderCell>
+            <Table.HeaderCell>2 Approved</Table.HeaderCell>
+            <Table.HeaderCell />
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    );
+  }
+}
