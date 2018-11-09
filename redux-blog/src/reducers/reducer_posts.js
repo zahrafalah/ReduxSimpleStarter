bcfa6860d53d264e6a93d44e6c81db44e64b454d
zahra falah <sepidehfallah2@gmail.com>
2018-11-09 14:11:06 -0700

    reducer commented

--- redux-blog/src/reducers/reducer_posts.js
+++ redux-blog/src/reducers/reducer_posts.js
@@ -1,6 +1,11 @@
+//we are using lodash lib to transfer the arr of data from payload to an object
+//with key & value based on our needs.[{},{},{}]
 import _ from 'lodash';
+//importing data from action creator
 import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
+//Our goal here inside this reducer is to return an object that contains the ID of every
+//post as the key and then the value will be the actual post itself.
 export default function(state = {}, action) {
   switch (action.type) {
     case DELETE_POST:
