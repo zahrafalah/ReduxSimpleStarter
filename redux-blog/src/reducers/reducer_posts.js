dcc3371e564db60b7a2bf41df4fc6c57e5b7b0f3
zahra falah <sepidehfallah2@gmail.com>
2018-11-15 15:51:47 -0700

    recieving new posts

--- redux-blog/src/reducers/reducer_posts.js
+++ redux-blog/src/reducers/reducer_posts.js
@@ -12,6 +12,10 @@ export default function(state = {}, action) {
     case DELETE_POST:
       return _.omit(state, action.payload);
     case FETCH_POST:
+      //... means lets fetch and add to any possible previous posts that
+      //has been fetched. Take all those and put it in this new obj.
+      //[]: by this we are not making a new arr, but key interpolation means we are asking to
+      //make a new key and set it to new value.then add them to the overall state
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
