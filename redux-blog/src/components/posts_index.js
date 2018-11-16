1ee155fdd66b0581b582858cd3e0c0a068afb679
zahra falah <sepidehfallah2@gmail.com>
2018-11-15 17:08:56 -0700

    cashing data

--- redux-blog/src/components/posts_index.js
+++ redux-blog/src/components/posts_index.js
@@ -15,7 +15,11 @@ class PostsIndex extends Component {
 
   renderPosts() {
     return _.map(this.props.posts, post => {
-      return <li key={post.id}>{post.title}</li>;
+      return (
+        <li key={post.id}>
+          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+        </li>
+      );
     });
   }
 
--- redux-blog/src/components/posts_show.js
+++ redux-blog/src/components/posts_show.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 import { Button, Form } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
   //ComponentDidMount would be automatically call once the component is rendered to the dom
   componentDidMount() {
-    const { id } = this.props.match.params;
-    //the action creator will fetch the post and will add it into post piece of state
-    //we then need to get out the post from state and pass it to the component with mapStateToProps func
-    this.props.fetchPost(id);
+    if (!this.props.post) {
+      const { id } = this.props.match.params;
+      //the action creator will fetch the post and will add it into post piece of state
+      //we then need to get out the post from state and pass it to the component with mapStateToProps func
+      this.props.fetchPost(id);
+    }
   }
 
   render() {
@@ -19,6 +22,7 @@ class PostsShow extends Component {
     }
     return (
       <div>
+        <Link to="/">Back To Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
