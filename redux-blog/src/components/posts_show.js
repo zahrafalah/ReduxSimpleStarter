bee116c16cfabf87b8ccfb487eaec7509268d230
zahra falah <sepidehfallah2@gmail.com>
2018-11-15 17:24:05 -0700

    delete a post

--- redux-blog/src/components/posts_show.js
+++ redux-blog/src/components/posts_show.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 import { Button, Form } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
   //ComponentDidMount would be automatically call once the component is rendered to the dom
   componentDidMount() {
-    if (!this.props.post) {
-      const { id } = this.props.match.params;
-      //the action creator will fetch the post and will add it into post piece of state
-      //we then need to get out the post from state and pass it to the component with mapStateToProps func
-      this.props.fetchPost(id);
-    }
+    // if (!this.props.post) {
+    const { id } = this.props.match.params;
+    //the action creator will fetch the post and will add it into post piece of state
+    //we then need to get out the post from state and pass it to the component with mapStateToProps func
+    this.props.fetchPost(id);
+    // }
+  }
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -23,6 +29,7 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back To Index</Link>
+        <Button onClick={this.onDeleteClick.bind(this)}>Delete</Button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -43,5 +50,5 @@ function mapStateToProps({ posts }, ownProps) {
 
 export default connect(
   mapStateToProps,
-  { fetchPost }
+  { fetchPost, deletePost }
 )(PostsShow);
